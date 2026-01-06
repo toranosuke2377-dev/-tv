bfa259adcaa10f7a3cd9a05f6991020c8c4f5f65
toranosuke2377-dev <toranosuke2377@gmail.com>
2026-01-06 18:26:02 +0900

    Upload src/app/debug/page.tsx

---
+++ src/app/debug/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function DebugPage() {
+    return (
+        <div className="min-h-screen bg-red-500 flex items-center justify-center">
+            <h1 className="text-6xl font-black text-white p-10 bg-black rounded-3xl shadow-2xl">
+                Tailwind Debug: Works if Background is Red
+            </h1>
+        </div>
+    );
+}
